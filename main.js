e529e6672c2c989cd409467ed669cc89d4818541
alexbotha <alexbotha@hotmail.co.uk>
2022-08-03 19:17:06 +0100

    commit tree e7f02ae6025339e2c9540cee16752441265cf2ca

--- main.js
+++ main.js
@@ -2,8 +2,49 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
+const errorModal = document.querySelector('#modal')
+
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  errorModal.classList.add('hidden')
+  clickListener()
+})
+
+
+function hideError(){
+  errorModal.classList.add("hidden")
+}
+
+function clickListener(){
+  document.addEventListener('click', (event) => {
+    
+    if(event.target.classList[0] === 'like-glyph'){
+      
+
+    mimicServerCall()
+    .then((response) => {
+      const activated = event.target.classList.contains("activated-heart")
+      if (activated) {
+      event.target.classList.remove('activated-heart')
+      event.target.innerHTML = EMPTY_HEART;
+      } else {
+        event.target.classList.add('activated-heart')
+        event.target.innerHTML = FULL_HEART;
+      } 
+      
+      
+    })
+    .catch((error) => {
+      errorModal.remove('hidden')
+      setTimeout(() => {
+      hideError()
+      }, 3000)
+    });
+
+}
+  })
+}
 
 
 
